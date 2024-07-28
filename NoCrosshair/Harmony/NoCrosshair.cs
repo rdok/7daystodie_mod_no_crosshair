d9f4c0a2ee099b99513b27c7b82650719c2a00ee
Rizart Dokollari <r.dokollari@gmail.com>
2024-07-28 21:18:40 +0100

    feat: Hide crosshair.

---
+++ NoCrosshair/Harmony/NoCrosshair.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+public class NoCrosshair
+{
+    public class NoCrosshairInit : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(ItemClass))]
+    [HarmonyPatch("GetCrosshairType")]
+    public class NoCrosshair_ItemClass_Crosshair
+    {
+        private static ItemClass.EnumCrosshairType Postfix(ItemClass.EnumCrosshairType __result)
+        {
+            return ItemClass.EnumCrosshairType.None;
+        }
+    }
+}
